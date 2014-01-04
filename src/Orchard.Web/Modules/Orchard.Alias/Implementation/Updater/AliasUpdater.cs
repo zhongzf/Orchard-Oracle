01fce36ac1ff196603de97c470b3850c38a3f3c8
zhongzf <zhongzf@126.com>
2014-01-04 21:02:10 +0800

    Orchard SQLite v.1.7.2 source
    1. Upgrade NHibernate to v3.3.3
    2. Add lib csharp-sqlite to support SQLite
    3. Replace oracle client to managed odp.net

---
+++ src/Orchard.Web/Modules/Orchard.Alias/Implementation/Updater/AliasUpdater.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Orchard.Alias.Implementation.Holder;
+using Orchard.Alias.Implementation.Storage;
+using Orchard.Environment;
+using Orchard.Tasks;
+using Orchard.Logging;
+using Orchard.Environment.Configuration;
+using Orchard.Data.Providers;
+
+namespace Orchard.Alias.Implementation.Updater {
+    public class AliasHolderUpdater : IOrchardShellEvents, IBackgroundTask {
+        private readonly IAliasHolder _aliasHolder;
+        private readonly IAliasStorage _storage;
+        private readonly IAliasUpdateCursor _cursor;
+        private readonly ShellSettings _shellSettings;
+
+        public ILogger Logger { get; set; }
+
+        public AliasHolderUpdater(IAliasHolder aliasHolder, IAliasStorage storage, IAliasUpdateCursor cursor, ShellSettings shellSettings) {
+            _aliasHolder = aliasHolder;
+            _storage = storage;
+            _cursor = cursor;
+			_shellSettings = shellSettings;
+            Logger = NullLogger.Instance;
+        }
+
+        void IOrchardShellEvents.Activated() {
+            Refresh();
+        }
+
+        void IOrchardShellEvents.Terminating() {
+        }
+
+        private void Refresh() {
+            try {
+                // only retreive aliases which have not been processed yet
+                var aliases = _storage.List(x => x.Id > _cursor.Cursor).ToArray();
+
+                // update the last processed id
+                if (aliases.Any()) {
+                    _cursor.Cursor = aliases.Last().Item5;
+                }                
+
+                if (_shellSettings.DataProvider == OracleDataServicesProvider.ProviderName)
+                {
+                    _aliasHolder.SetAliases(aliases.Select(alias => new AliasInfo { Path = alias.Item1 ?? string.Empty, Area = alias.Item2, RouteValues = alias.Item3 }));
+                }
+                else
+                {
+                    _aliasHolder.SetAliases(aliases.Select(alias => new AliasInfo { Path = alias.Item1, Area = alias.Item2, RouteValues = alias.Item3 }));
+                }
+            }
+            catch (Exception ex) {
+                Logger.Error(ex, "Exception during Alias refresh");
+            }
+        }
+
+        public void Sweep() {
+            Refresh();
+        }
+    }
+}
